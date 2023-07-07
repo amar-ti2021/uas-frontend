1d67549d77c98db20078f8ae67ff02e0086f57ce
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 17:20:50 +0700

    feat: Create GlobalSection component
    - Create GlobalSection.tsx and index.tsx files in the src/components/GlobalSection directory
    
    This commit introduces the creation of the GlobalSection component in the project. The GlobalSection component is responsible for displaying global section content, providing an overview of global statistics or information relevant to the entire application. The GlobalSection.tsx and index.tsx files have been created in the src/components/GlobalSection directory to implement this component. This addition contributes to the overall structure and functionality of the application.

---
+++ src/components/GlobalSection/GlobalSection.tsx
@@ -0,0 +1,34 @@
+import Subtitle from "../../lib/ui/Subtitle/Subtitle";
+import Title from "../../lib/ui/Title/Title";
+import Card from "../Card";
+import Section from "../Section";
+import Stack from "../Stack";
+
+interface GlobalSectionProps {
+  global: [
+    {
+      status: string;
+      total: number;
+    }
+  ];
+}
+
+const GlobalSection = ({ global }: GlobalSectionProps) => {
+  return (
+    <Section
+      title="Global Situation"
+      subtitle="Data Covid Berdasarkan Status Global"
+    >
+      <Stack>
+        {global.map((item) => (
+          <Card>
+            <Title>{item.status}</Title>
+            <Subtitle>{item.total}</Subtitle>
+          </Card>
+        ))}
+      </Stack>
+    </Section>
+  );
+};
+
+export default GlobalSection;
