2ef5049a1d4d3f00f13b13418ad3da080577448e
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 23:08:39 +0700

    feat: Extract Logo element from Navbar component for reuse
    This commit extracts the Logo element from the Navbar component to make it reusable across multiple components. The Logo component consists of the following files:
    - Logo.tsx: This file contains the implementation of the Logo component.
    - LogoStyled.tsx: This file contains the styled components specific to the Logo component.
    - index.tsx: This file exports the Logo component for easier importing.
    
    Changes Made:
    - Added Logo.tsx
    - Added LogoStyled.tsx
    - Added index.tsx

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import { LogoIcon } from "../../assets/icons";
+import LogoStyled from "./LogoStyled";
+
+const Logo = () => {
+  return (
+    <LogoStyled>
+      <LogoIcon color="#f5f5f5" />
+      <h1>COVID ID</h1>
+    </LogoStyled>
+  );
+};
+
+export default Logo;
