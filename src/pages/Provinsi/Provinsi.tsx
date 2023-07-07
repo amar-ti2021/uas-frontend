dfc7fcbf16d891bad618e12e631bc79b316cafa7
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 18:04:13 +0700

    Copy code feat: Create Provinsi Page to Display Provinces Table and Update Form
    This commit introduces a new Provinsi page to the application. The Provinsi page is responsible for displaying a table of provinces and an update form to modify province information.
    
    Changes Made:
    - Added Provinsi.tsx: This file contains the implementation of the Provinsi page.
    - Added index.tsx: This file exports the Provinsi page for easier importing.

---
+++ src/pages/Provinsi/Provinsi.tsx
@@ -0,0 +1,13 @@
+import ProvinceSection from "../../components/ProvinceSection";
+import UpdateForm from "../../components/UpdateForm";
+
+const Provinsi = () => {
+  return (
+    <>
+      <ProvinceSection />
+      <UpdateForm />
+    </>
+  );
+};
+
+export default Provinsi;
