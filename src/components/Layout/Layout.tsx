40bd68c7ba0a9bc653754587d6e82d212a5f48a0
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 17:28:01 +0700

    feat: Add Hero, Outlet, and Footer to Layout component
    - Modify Layout.tsx to include Hero, Outlet, and Footer components
    
    This commit introduces the addition of the Hero, Outlet, and Footer components to the Layout component in the project. The Layout component serves as a common wrapper or container for the application's main content. By including the Hero component, it allows for the display of a prominent section at the top of the layout, typically used for showcasing key information or visuals. The Outlet component enables dynamic rendering of different sections or content within the layout. Lastly, the Footer component adds a footer section at the bottom of the layout, providing additional information, navigation, or other relevant content.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,17 @@
-import Navbar from "../Navbar"
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar";
+import Hero from "../Hero";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
