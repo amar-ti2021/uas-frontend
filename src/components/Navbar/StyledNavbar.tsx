afd36eec1024f29aa2278b284df444b1f53db7e2
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 23:45:52 +0700

    refactor: Change styles in StyledNavbar component
    This commit refactors the styles in the StyledNavbar component. The changes include:
    - Adjusting the justify-content property to "space-between" for improved spacing.
    - Removing the position and margin properties for simplification.
    - Removing the ul and li styles since they are no longer used.
    
    Changes Made:
    - Modified: src/components/Navbar/StyledNavbar.tsx

--- src/components/Navbar/StyledNavbar.tsx
+++ src/components/Navbar/StyledNavbar.tsx
@@ -2,26 +2,8 @@ import styled from "styled-components";
 
 const StyledNavbar = styled.nav`
   display: flex;
-  justify-content: space-around;
-  position: sticky;
-  padding: 0.3rem;
-  > * {
-    margin: 0;
-    display: flex;
-    align-items: center;
-  }
-  ul {
-    display: flex;
-    justify-content: space-around;
-    list-style-type: none;
-    li {
-      padding: 0 0.3rem;
-      > * {
-        color: var(--white);
-        text-decoration: none;
-      }
-    }
-  }
+  justify-content: space-between;
+  padding: 0.8rem;
 `;
 
 export default StyledNavbar;
