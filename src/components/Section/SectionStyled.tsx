1b9d0707c49bad3bb4c3a8361aa25b4a40f86fb5
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 23:50:53 +0700

    finish uas-frontend

--- src/components/Section/SectionStyled.tsx
+++ src/components/Section/SectionStyled.tsx
@@ -2,7 +2,11 @@ import { styled } from "styled-components";
 
 const SectionStyled = styled.section`
   min-height: 100vh;
+  display: 100vw;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   padding: 3rem;
 `;
 export default SectionStyled;
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,19 +1,47 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import StyledTable from "./StyledTable";
 
 interface TableProps {
   header: string[];
   children: ReactNode;
+  selectedStatus: string;
+  setSelectedStatus: React.Dispatch<React.SetStateAction<string>>;
 }
-const Table = ({ header, children }: TableProps) => {
+const Table = ({
+  header,
+  children,
+  selectedStatus,
+  setSelectedStatus,
+}: TableProps) => {
+  const status = ["confirmed", "recovered", "treatment", "death"];
+  const [statusIndex, setStatusIndex] = useState(0);
+
+  const changeSelectedStatus = () => {
+    const nextIndex = (statusIndex + 1) % status.length;
+    const nextStatus = status[nextIndex];
+    setSelectedStatus(nextStatus);
+    setStatusIndex(nextIndex);
+  };
   return (
-    <table>
-      <tr>
-        {header.map((item) => (
-          <th>{item}</th>
-        ))}
-      </tr>
-      {children}
-    </table>
+    <StyledTable>
+      <thead>
+        <tr>
+          {header.map((item, id) => (
+            <th key={id}>{item}</th>
+          ))}
+          <th onClick={() => changeSelectedStatus()}>{selectedStatus}</th>
+        </tr>
+      </thead>
+      <tbody>{children}</tbody>
+      <tfoot>
+        <tr>
+          {header.map((item, id) => (
+            <th key={id}>{item}</th>
+          ))}
+          <th onClick={() => changeSelectedStatus()}>{selectedStatus}</th>
+        </tr>
+      </tfoot>
+    </StyledTable>
   );
 };
 
