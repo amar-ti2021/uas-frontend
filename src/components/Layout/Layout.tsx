bf60fda0c298b5e7811214c5122701fca26b6e7f
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 23:43:42 +0700

    feat: Modify Layout component to conditionally render Hero based on the route
    This commit updates the Layout component to conditionally render the Hero component based on the current route. The changes include:
    - Importing `useLocation` from react-router-dom to access the current location.
    - Checking if the current `location.pathname` is not equal to "/about" to determine whether to render the Hero component or not.
    
    Changes Made:
    - Modified: src/components/Layout/Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Navbar";
 import Hero from "../Hero";
 import Footer from "../Footer/Footer";
 
 const Layout = () => {
+  const location = useLocation();
   return (
     <>
       <Navbar />
-      <Hero />
+      {location.pathname !== "/about" ? <Hero /> : ""}
       <Outlet />
       <Footer />
     </>
