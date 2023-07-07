11ebd873929080d8bd0ab460b200a9c815c2d68c
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 18:07:36 +0700

    feat: Add react-redux Provider to App.tsx
    This commit adds the react-redux Provider component to the App.tsx file. The Provider component is responsible for providing the Redux store to all components in the application.
    
    Changes Made:
    - Modified: src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './lib/router'
+import { RouterProvider } from "react-router-dom";
+import router from "./lib/router";
+import { Provider } from "react-redux";
+import store from "./lib/store/store";
 
 function App() {
-
   return (
-    <RouterProvider router={router} />
-  )
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
