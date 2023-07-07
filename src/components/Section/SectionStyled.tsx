98631d97198ccba9144162a3e7cbcdee0484eaa2
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-07-07 17:35:10 +0700

    feat: Create Section component as a container for sections
    This commit introduces the Section component, which serves as a container for different sections within the application. The Section component provides a consistent layout and styling for each section, ensuring a cohesive and organized user interface.

---
+++ src/components/Section/SectionStyled.tsx
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+const SectionStyled = styled.section`
+  min-height: 100vh;
+  text-align: center;
+  padding: 3rem;
+`;
+export default SectionStyled;
